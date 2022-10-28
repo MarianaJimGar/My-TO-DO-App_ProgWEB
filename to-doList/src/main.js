3919675d7fa829947b7cb82308d6f80bd752183b
MarianaJimGar <mariianajimenezgarcia@hotmail.com>
2022-10-28 03:00:45 -0500

    update
    update

--- to-doList/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import '@fortawesome/fontawesome-free/css/all.css'
-import '@fortawesome/fontawesome-free/js/all.js'
-
-import './assets/main.css'
-
-createApp(App).mount('#app')
